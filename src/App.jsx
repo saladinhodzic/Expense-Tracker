06d961306ed8921bc6ffb90d171aa674d4262216
saladinhodzic <hsaladin06@gmail.com>
2024-08-19 10:44:20 +0200

    Adding box shadow and improving styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage/LandingPage";
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <LandingPage />
+      </main>
       <Footer />
     </>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,11 @@
 import "./Footer.css";
 export default function Footer() {
-  return;
+  return (
+    <footer>
+      <div className="footer-wrapper">
+        <h2>Expense Tracker</h2>
+        <p>Copyright @2024</p>
+      </div>
+    </footer>
+  );
 }
