90a4e07f225da5798e039ff238fe8297f2b864da
saladinhodzic <hsaladin06@gmail.com>
2024-08-20 14:07:49 +0200

    Making landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,1 +1,16 @@
-export default function LandingPage() {}
+import "./LandingPage.css";
+export default function LandingPage() {
+  return (
+    <div className="landing-page">
+      <div className="landing-page-wrapper">
+        <h1 className="landing-page-title">
+          Find how much can you save in a month
+        </h1>
+        <p>
+          Using our app you can calculate your daily expenses and savings{" "}
+          <button>Try it now</button>
+        </p>
+      </div>
+    </div>
+  );
+}
