cc6673403b93280845f11c73d20f56dbb0295c22
saladinhodzic <hsaladin06@gmail.com>
2024-08-21 14:35:20 +0200

    if user not logged in just show button login in the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,29 @@
+import { useContext } from "react";
 import "./Header.css";
+import { AppContext } from "../../context/ContextWrapper";
 
 export default function Header() {
+  const { loggedIn } = useContext(AppContext);
+  let list = (
+    <ul className="list">
+      <li>Profile</li>
+      <li>Your Savings</li>
+      <li>Your expenses</li>
+    </ul>
+  );
+
+  if (!loggedIn) {
+    list = (
+      <ul className="list">
+        <button>Login</button>
+      </ul>
+    );
+  }
   return (
     <header>
       <div className="header-wrapper">
         <h1>Expense Tracker</h1>
-        <ul className="list">
-          <li>Profile</li>
-          <li>Your Savings</li>
-          <li>Your expenses</li>
-        </ul>
+        {list}
       </div>
     </header>
   );
