45d7c26b0832c07d7a4e1eb7274b0a5a54255f1d
saladinhodzic <hsaladin06@gmail.com>
2024-08-24 17:41:06 +0200

    formik works just need to add styles

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -30,6 +30,22 @@ export default function Login() {
           onChange={formik.handleChange}
           value={formik.values.username}
         />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
 
         <button type="submit">Submit</button>
       </form>
