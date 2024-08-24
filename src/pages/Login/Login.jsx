05361395cc167df4f80998971f6a2b71434fc4a8
saladinhodzic <hsaladin06@gmail.com>
2024-08-24 02:39:53 +0200

    Making the validation schema with yup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,1 +1,10 @@
-export default function Login() {}
+import "./Login.css";
+import { Formik } from "formik";
+import * as Yup from "yup";
+export default function Login() {
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(2, "Too short.").max(12, "Too long."),
+    email: Yup.string().email("Invalid email.").required("Requried."),
+    password: Yup.string().min(6, "Password too short.").required("Required."),
+  });
+}
