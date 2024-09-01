8b4f2ab059e51d3a24345f089881f2e9c498802c
saladinhodzic <hsaladin06@gmail.com>
2024-09-01 12:29:07 +0200

    Starting the homepage

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,2 +1,13 @@
 import "./Home.css";
-export default function Home() {}
+export default function Home() {
+  const username = JSON.parse(localStorage.getItem("user"));
+
+  return (
+    <div className="home-page-wrapper">
+      <section className="welcome-section">
+        <h1>Welcome to the Expense Tracker, {username.username}.</h1>
+      </section>
+      <section className="enter-info-section"></section>
+    </div>
+  );
+}
