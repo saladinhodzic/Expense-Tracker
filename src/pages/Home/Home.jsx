54097c70a14b6ac7b317694a64954a7dc88b422c
saladinhodzic <hsaladin06@gmail.com>
2024-09-02 23:39:56 +0200

    making some changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <div className="home-page-wrapper">
       <section className="welcome-section">
-        <h1>Welcome to the Expense Tracker, {username.username}.</h1>
+        <h1>Welcome to Expense Tracker, {username.username}.</h1>
+        <h3>With us you can find out how much you earn, save and spend.</h3>
       </section>
       <section className="enter-info-section"></section>
     </div>
